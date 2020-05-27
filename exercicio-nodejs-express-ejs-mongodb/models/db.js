7420b5d488b4479fc94377a2a76d018fbc54858f
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-05-27 17:08:28 -0300

    Ajustado nome do database

--- exercicio-nodejs-express-ejs-mongodb/models/db.js
+++ exercicio-nodejs-express-ejs-mongodb/models/db.js
@@ -4,7 +4,7 @@ const objectId = require("mongodb").ObjectId;
 function conectar(callback) {
     const mongoClient = new MongoClient(process.env.MONGO_CONNECTION,  { useUnifiedTopology: true });
     mongoClient.connect()
-        .then(conn => conn.db("curso-node-mongo-luiz-tools"))
+        .then(conn => conn.db("curso-nodejs-mongodb-luiz-tools"))
         .then(db => global.conn = db)
         .then(callback())
         .catch(err => console.log(err));
