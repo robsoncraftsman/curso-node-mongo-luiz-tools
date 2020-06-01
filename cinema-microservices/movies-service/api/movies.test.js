154693c95a3176e5e09c56769125cb2b77d98b53
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-06-01 17:26:47 -0300

    Ajustado para usar finally para fechar conexões

--- cinema-microservices/movies-service/api/movies.test.js
+++ cinema-microservices/movies-service/api/movies.test.js
@@ -6,56 +6,68 @@ const moviesRepository = require("../repository/moviesRepository");
 
 function runTests() {
   test("GET /movies", async (t) => {
-    return server.start(movies, moviesRepository).then(async (app) => {
-      const res = await new Promise((resolve, reject) => {
-        supertest(app)
-          .get("/movies")
-          .expect("Content-Type", /json/)
-          .expect(200)
-          .end((err, res) => {
-            resolve(res);
-          });
+    return server
+      .start(movies, moviesRepository)
+      .then(async (app) => {
+        const res = await new Promise((resolve, reject) => {
+          supertest(app)
+            .get("/movies")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end((err, res) => {
+              resolve(res);
+            });
+        });
+        t.assert(res.body && res.body.length > 0, "All Movies returned");
+      })
+      .finally(async () => {
+        await moviesRepository.disconnect();
+        await server.stop();
       });
-      t.assert(res.body && res.body.length > 0, "All Movies returned");
-      await moviesRepository.disconnect();
-      await server.stop();
-    });
   });
 
   test("GET /movies/:id", async (t) => {
-    return server.start(movies, moviesRepository).then(async (app) => {
-      const res = await new Promise((resolve, reject) => {
+    return server
+      .start(movies, moviesRepository)
+      .then(async (app) => {
         const id = "5ed0713c380e6d955fcf4406";
-        supertest(app)
-          .get("/movies/" + id)
-          .expect("Content-Type", /json/)
-          .expect(200)
-          .end((err, res) => {
-            resolve(res);
-          });
-      });
+        const res = await new Promise((resolve, reject) => {
+          supertest(app)
+            .get("/movies/" + id)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end((err, res) => {
+              resolve(res);
+            });
+        });
 
-      t.assert(res.body && res.body._id === id, "Movies By Id returned");
-      await moviesRepository.disconnect();
-      await server.stop();
-    });
+        t.assert(res.body && res.body._id === id, "Movies By Id returned");
+      })
+      .finally(async () => {
+        await moviesRepository.disconnect();
+        await server.stop();
+      });
   });
 
   test("GET /movies/premieres", async (t) => {
-    return server.start(movies, moviesRepository).then(async (app) => {
-      const res = await new Promise((resolve, reject) => {
-        supertest(app)
-          .get("/movies/premieres")
-          .expect("Content-Type", /json/)
-          .expect(200)
-          .end((err, res) => {
-            resolve(res);
-          });
+    return server
+      .start(movies, moviesRepository)
+      .then(async (app) => {
+        const res = await new Promise((resolve, reject) => {
+          supertest(app)
+            .get("/movies/premieres")
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end((err, res) => {
+              resolve(res);
+            });
+        });
+        t.assert(res.body && res.body.length > 0, "Premiere Movies returned");
+      })
+      .finally(async () => {
+        await moviesRepository.disconnect();
+        await server.stop();
       });
-      t.assert(res.body && res.body.length > 0, "Premiere Movies returned");
-      await moviesRepository.disconnect();
-      await server.stop();
-    });
   });
 }
 
