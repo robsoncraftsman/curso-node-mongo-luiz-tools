63aa84d005aae7c44c0c12a03579f739d08ec59c
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-06-03 17:04:09 -0300

    Ajustado para reduzir codigo duplicado nos testes da api

--- cinema-microservices/movies-service/api/movies.test.js
+++ cinema-microservices/movies-service/api/movies.test.js
@@ -5,99 +5,87 @@ const server = require("../server/server");
 const repository = require("../repository/repository");
 
 function runTests() {
-  test("POST /movies", async (t) => {
+  async function executeAsyncApiTest(fn) {
     return server
       .start(movies, repository)
       .then(async (app) => {
-        const res = await new Promise((resolve, reject) => {
-          supertest(app)
-            .post("/movies")
-            .send({
-              titulo: "Vingadores",
-              sinopse: "Heróis mais poderosos da Terra",
-              duracao: 120,
-              dataLancamento: new Date(),
-              imagem:
-                "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
-              categorias: ["Aventura"],
-            })
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .end((err, res) => {
-              resolve(res);
-            });
-        });
-        t.assert(res && res.body.insertedCount > 0, "Movie Added");
+        return fn(app);
       })
       .finally(async () => {
         await repository.disconnect();
         await server.stop();
       });
+  }
+
+  test("POST /movies", async (t) => {
+    return executeAsyncApiTest(async (app) => {
+      const res = await new Promise((resolve, reject) => {
+        supertest(app)
+          .post("/movies")
+          .send({
+            titulo: "Vingadores",
+            sinopse: "Heróis mais poderosos da Terra",
+            duracao: 120,
+            dataLancamento: new Date(),
+            imagem:
+              "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
+            categorias: ["Aventura"],
+          })
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .end((err, res) => {
+            resolve(res);
+          });
+      });
+      t.assert(res && res.body.insertedCount > 0, "Movie Added");
+    });
   });
 
   test("GET /movies", async (t) => {
-    return server
-      .start(movies, repository)
-      .then(async (app) => {
-        const res = await new Promise((resolve, reject) => {
-          supertest(app)
-            .get("/movies")
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .end((err, res) => {
-              resolve(res);
-            });
-        });
-        t.assert(res.body && res.body.length > 0, "All Movies returned");
-      })
-      .finally(async () => {
-        await repository.disconnect();
-        await server.stop();
+    return executeAsyncApiTest(async (app) => {
+      const res = await new Promise((resolve, reject) => {
+        supertest(app)
+          .get("/movies")
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .end((err, res) => {
+            resolve(res);
+          });
       });
+      t.assert(res.body && res.body.length > 0, "All Movies returned");
+    });
   });
 
   test("GET /movies/:id", async (t) => {
-    return server
-      .start(movies, repository)
-      .then(async (app) => {
-        const id = "5ed0713c380e6d955fcf4406";
-        const res = await new Promise((resolve, reject) => {
-          supertest(app)
-            .get("/movies/" + id)
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .end((err, res) => {
-              resolve(res);
-            });
-        });
-
-        t.assert(res.body && res.body._id === id, "Movies By Id returned");
-      })
-      .finally(async () => {
-        await repository.disconnect();
-        await server.stop();
+    return executeAsyncApiTest(async (app) => {
+      const id = "5ed0713c380e6d955fcf4406";
+      const res = await new Promise((resolve, reject) => {
+        supertest(app)
+          .get("/movies/" + id)
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .end((err, res) => {
+            resolve(res);
+          });
       });
+
+      t.assert(res.body && res.body._id === id, "Movies By Id returned");
+    });
   });
 
   test("GET /movies/premieres", async (t) => {
-    return server
-      .start(movies, repository)
-      .then(async (app) => {
-        const res = await new Promise((resolve, reject) => {
-          supertest(app)
-            .get("/movies/premieres")
-            .expect("Content-Type", /json/)
-            .expect(200)
-            .end((err, res) => {
-              resolve(res);
-            });
-        });
-        t.assert(res.body && res.body.length > 0, "Premiere Movies returned");
-      })
-      .finally(async () => {
-        await repository.disconnect();
-        await server.stop();
+    return executeAsyncApiTest(async (app) => {
+      const res = await new Promise((resolve, reject) => {
+        supertest(app)
+          .get("/movies/premieres")
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .end((err, res) => {
+            resolve(res);
+          });
       });
+      t.assert(res.body && res.body.length > 0, "Premiere Movies returned");
+    });
   });
 }
 
