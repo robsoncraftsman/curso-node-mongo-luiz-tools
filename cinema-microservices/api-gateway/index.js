f4f5265aebec178cd146bf5f0d3d04145cc272ab
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-06-04 15:42:35 -0300

    Adicionado login via JWT

--- cinema-microservices/api-gateway/index.js
+++ cinema-microservices/api-gateway/index.js
@@ -1,3 +1,4 @@
+require("dotenv-safe").config();
 const http = require("http");
 const express = require("express");
 const httpProxy = require("express-http-proxy");
@@ -5,21 +6,10 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const helmet = require("helmet");
+const jwt = require("jsonwebtoken");
 
 const moviesServiceProxy = httpProxy("http://localhost:3010");
 const cinemaCatalogServiceProxy = httpProxy("http://localhost:3020");
-
-app.get("/movies(?:$|/*)", (req, res, next) => {
-  moviesServiceProxy(req, res, next);
-});
-
-app.get("/cities(?:$|/*)", (req, res, next) => {
-  cinemaCatalogServiceProxy(req, res, next);
-});
-
-app.get("/cinemas(?:$|/*)", (req, res, next) => {
-  cinemaCatalogServiceProxy(req, res, next);
-});
 
 app.use(logger("dev"));
 app.use(helmet());
@@ -27,5 +17,51 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-var server = http.createServer(app);
+function verifyJWT(req, res, next) {
+  var token = req.headers["x-access-token"];
+  if (!token)
+    return res.status(401).send({ auth: false, message: "No token provided" });
+
+  jwt.verify(token, process.env.SECRET, function (err, decoded) {
+    if (err)
+      return res
+        .status(401)
+        .send({ auth: false, message: "Failed to authenticate token" });
+
+    req.userId = decoded.id;
+    next();
+  });
+}
+
+app.post("/login", (req, res, next) => {
+  const userId = req.body.user;
+  const pwd = req.body.pwd;
+  if (userId === "JOAO" && pwd === "123") {
+    var token = jwt.sign({ id: userId }, process.env.SECRET, {
+      expiresIn: 300, // expires in 5min
+    });
+
+    res.status(200).send({ auth: true, token: token });
+  } else {
+    res.status(401).send("Invalid login");
+  }
+});
+
+app.get("/logout", function (req, res) {
+  res.status(200).send({ auth: false, token: null });
+});
+
+app.get("/movies(?:$|/*)", verifyJWT, (req, res, next) => {
+  moviesServiceProxy(req, res, next);
+});
+
+app.get("/cities(?:$|/*)", verifyJWT, (req, res, next) => {
+  cinemaCatalogServiceProxy(req, res, next);
+});
+
+app.get("/cinemas(?:$|/*)", verifyJWT, (req, res, next) => {
+  cinemaCatalogServiceProxy(req, res, next);
+});
+
+const server = http.createServer(app);
 server.listen(3000);
